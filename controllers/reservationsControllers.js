9c1bfd301a0cae26375f1ac2f8bbde9fa7f9559f
Alethepata <alessia.patane@gmail.com>
2024-05-31 15:08:56 +0200

    add reservations

---
+++ controllers/reservationsControllers.js
@@ -0,0 +1,17 @@
+const index = (req, res) => {
+    res.end();
+}
+
+const store = (req, res) => {
+    res.end();
+}
+
+const destroy = (req, res) => {
+    res.end();
+}
+
+module.exports= {
+    index,
+    store,
+    destroy
+}
--- routers/events.js
+++ routers/events.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const {index, store, update} = require('../controllers/eventsController.js');
+const { index, store, update } = require('../controllers/eventsController.js');
+
+const reservationsControllers = require('../controllers/reservationsControllers.js');
 
 router.get('/', index);
 
 router.post('/', store);
 
 router.put('/:id', update);
+
+router.get('/:id/reservations', reservationsControllers.index)
+
+router.post('/:id/reservations', reservationsControllers.store)
+
+router.delete('/:id/reservations/:reservationsId', reservationsControllers.destroy)
 
 module.exports = router;
