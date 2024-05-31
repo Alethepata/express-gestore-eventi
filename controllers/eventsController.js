4efc18fc08575812f3df75f3f413b5a6524a7487
Alethepata <alessia.patane@gmail.com>
2024-05-31 14:24:42 +0200

    add filter

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -2,6 +2,19 @@ const Event = require('../models/events.js');
 
 const index = (req, res) => {
     const events = Event.events();
+
+    const titleFilter = req.query.title;
+    const dateFilter = req.query.date;
+
+    if (titleFilter) {
+        const event = events.filter(event => event.title.toLowerCase() === titleFilter);
+        return res.json(event);  
+    }else if (dateFilter) {
+        const event = events.filter(event => event.date === dateFilter);
+        return res.json(event);  
+    }
+
+
     res.json(events);
 }
 
@@ -11,7 +24,7 @@ const store = (req, res) => {
     const newEvents = new Event(id, title, description, date, maxSeats);
 
     Event.add(newEvents);
-    
+
     res.redirect('/events');
 }
 
