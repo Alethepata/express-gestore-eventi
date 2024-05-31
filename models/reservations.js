d1e17949997d32fbcf9c91d94b64b5fd7249f3ff
Alethepata <alessia.patane@gmail.com>
2024-05-31 17:34:37 +0200

    add static method

--- models/reservations.js
+++ models/reservations.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+
 class Reservation {
     constructor(id, firstName, lastName, email, eventId) {
         this.id = id;
@@ -6,6 +9,25 @@ class Reservation {
         this.email = email;
         this.eventId = eventId;
     }
+
+    static read() {
+        const filePath = path.join(__dirname, '../db/reservations.json');
+        const reservation = fs.readFileSync(filePath, "utf-8");
+        return JSON.parse(reservation);
+    }
+
+    static reservation(id) {
+        const reservation = this.read().filter(reservation => reservation.eventId == id);
+        console.log(reservation);
+        return reservation
+    }
+    
+    static add(newReservation){
+        const filePath = path.join(__dirname, '../db/reservations.json');
+        fs.writeFileSync(filePath, JSON.stringify([this.reservation(), newReservation]));
+    }
+
+
 }
 
 module.exports = Reservation;
